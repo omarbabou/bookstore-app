d37e7bd5330aa4342a2db8068cf82c9bd39189d1
omarbabou <obabou50@gmail.com>
2022-08-17 15:12:35 +0200

    Add stylelint linter in my project

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux/es/exports';
 import store from './redux/configureStore';
-import './index.css';
 import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <Provider store={store()}>
     <App />
   </Provider>,
 );
