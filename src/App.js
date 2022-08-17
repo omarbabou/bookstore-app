b2565e66e0708f461c069ccc6cf337dc94534b58
omarbabou <obabou50@gmail.com>
2022-08-17 21:38:40 +0200

    Give styles to the navbar

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import Categories from './components/Categories';
 import { fetchBooks } from './redux/books/books';
 import Books from './components/Books';
+import './index.css';
 
 const App = () => {
   const state = useSelector((state) => state);
